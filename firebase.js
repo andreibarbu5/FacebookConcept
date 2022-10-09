a9611d6bc3fb882d5d5d814fa15f739d6fcf7f5a
BarbuAndreiIulian <andreibarbu681@gmail.com>
2022-10-09 19:13:33 +0300

    reverse-test

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_IDX,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
