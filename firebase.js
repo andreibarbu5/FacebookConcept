1867cedb1ac5091a09e7d79a4dc2d785bb8526ce
BarbuAndreiIulian <andreibarbu681@gmail.com>
2022-10-09 19:26:07 +0300

    test5

--- firebase.js
+++ firebase.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.X,
-  authDomain: process.env.X,
-  projectId: process.env.X,
-  storageBucket: process.env.X,
-  messagingSenderId: process.env.X,
-  appId: process.env.X,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+hi;
